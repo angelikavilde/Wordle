12109ca8a69d232591f60158dfa9e79d1ce18a10
Angela <angelikavilde@gmail.com>
2023-06-28 21:52:28 +0100

    table game finished with lost colours of the cells

--- wordle.py
+++ wordle.py
@@ -10,19 +10,16 @@ def open_word_list() -> any:
         word_data = json.load(file)
         return word_data
 
-def generate_random_word(word_data) -> list:
+def generate_random_word(word_data: list[dict]) -> list:
     i = random.randint(0,len(word_data))
     return word_data[i]
 
-def show_game(word: list, letters_exist: set, guess: str):
+def show_game(word: list[str], letters_exist: set[str], guess: str) -> Table:
     
-    table = Table(title = "Wordle", show_header=False, show_lines=True)
+    table = Table(show_header=False, show_lines=True)
 
-    table.add_column()
-    table.add_column()
-    table.add_column()
-    table.add_column()
-    table.add_column()
+    for _ in range (5):
+        table.add_column()
 
     table.add_row(guess[0],guess[1],guess[2],guess[3],guess[4])
 
@@ -34,10 +31,22 @@ def show_game(word: list, letters_exist: set, guess: str):
 
     return table
 
+def combine_tables(table_list:list[Table]) -> Table:
+    table = Table(title="Wordle", show_header=False, show_lines=True)
+    for _ in range(5):
+        table.add_column()
+    for t in table_list:
+        row_cells = []
+        for col in t.columns:
+            cel_list = list(col.cells)
+            row_cells.append(str(cel_list[0]))
+        table.add_row(*row_cells)
+    return table
+
 def guess_word():
     return input("Word guess: ")
 
-def check_word(guess: str):
+def check_word(guess: str) -> bool:
     letters_list = string.ascii_lowercase
 
     if len(guess) != 5:
@@ -47,7 +56,7 @@ def check_word(guess: str):
             return False
     return True
 
-def check_letters(answer: str, guess:str):
+def check_letters(answer: str, guess:str) -> tuple[list[str],set]:
     word = ["","","","",""]
     letters_exist = set()
     print(answer)
@@ -59,12 +68,12 @@ def check_letters(answer: str, guess:str):
                 letters_exist.add(letter)
     return word, letters_exist
 
-def confirm_answer(answer: str, guess:list) -> bool:
+def confirm_answer(answer: str, guess:list[str]) -> bool:
     if "".join(guess) == answer:
         return True
     return False
 
-def play(word_data) -> None:
+def play(word_data:list[dict]) -> None:
     random_word_dict = generate_random_word(word_data)
     random_word_dict = {"curls": "A piece or lock of curling hair; a ringlet."}
     random_word = list(random_word_dict.keys())[0]
@@ -72,6 +81,7 @@ def play(word_data) -> None:
     console.print("*", style="green", end=" - Correct place and letter\n")
     console.print("*", style="yellow", end=" - Correct letter only\n")
     guess = []
+    table_list = []
     while guesses < 5:
         guess = guess_word()
         print(guesses)
@@ -80,12 +90,15 @@ def play(word_data) -> None:
             continue
         word, letters_exist = check_letters(random_word, guess)
         if confirm_answer(random_word, word):
-            table = show_game(word, letters_exist, guess)
-            console.print(table)
+            table_list.append(show_game(word, letters_exist, guess))
+            table1 = combine_tables(table_list)
+            console.print(table1)
             print("You win!")
             return
         else:
-            show_game(word, letters_exist, guess)
+            table_list.append((show_game(word, letters_exist, guess)))
+            table1 = combine_tables(table_list)
+            console.print(table1)
             guesses += 1
     print("Game over!")
     print(f"The correct word was {random_word}")
